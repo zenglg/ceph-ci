908e6127f8553ba6280286efb68e7792ca519e72
Ali Maredia <amaredia@redhat.com>
2016-12-20 13:09:12 -0500

    rgw multisite: use get_config_master_client for radosgw_admin task
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- qa/tasks/radosgw_admin.py
+++ qa/tasks/radosgw_admin.py
@@ -68,18 +68,21 @@ def task(ctx, config):
     """
     global log
 
-    assert isinstance(config, dict), \
-        "task radosgw-admin only supports a dictionary with a master_client for configuration"
-
-    # regions found just like in the rgw task
+    # regions and config found from rgw task
     regions = ctx.rgw.regions
+    config = ctx.rgw.config
 
     log.debug('regions are: %r', regions)
     log.debug('config is: %r', config)
 
-    client = config["master_client"]
+    clients_from_config = config.keys()
+
+    # choose first client as default
+    client = clients_from_config[0]
 
     multi_region_run = rgw_utils.multi_region_enabled(ctx)
+    if multi_region_run:
+        client = rgw_utils.get_config_master_client(ctx, config, regions)
 
     log.debug('multi_region_run is: %r', multi_region_run)
     log.debug('master_client is: %r', client)
