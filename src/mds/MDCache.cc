807d0eccb6c638583e880077ca8702b580890cf6
John Spray <john.spray@redhat.com>
2016-12-21 11:22:30 +0000

    mds: add stats to PurgeQueue
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/mds/MDCache.cc
+++ src/mds/MDCache.cc
@@ -7602,7 +7602,7 @@ bool MDCache::shutdown_pass()
     show_cache();
     //dump();
     return false;
-  } 
+  }
   
   // done!
   dout(2) << "shutdown done." << dendl;
@@ -12306,14 +12306,12 @@ void MDCache::register_perfcounters()
     pcb.add_u64(l_mdc_num_strays_delayed, "num_strays_delayed", "Stray dentries delayed");
     pcb.add_u64(l_mdc_num_strays_enqueuing, "num_strays_enqueuing", "Stray dentries enqueuing for purge");
 
-    pcb.add_u64(l_mdc_num_strays_purging, "num_strays_purging", "Stray dentries purging");
-    pcb.add_u64(l_mdc_num_purge_ops, "num_purge_ops", "Purge operations");
-
     pcb.add_u64_counter(l_mdc_strays_created, "strays_created", "Stray dentries created");
     pcb.add_u64_counter(l_mdc_strays_enqueued, "strays_enqueued",
-        "Stray dentries enqueued for purge", "purg");
+        "Stray dentries enqueued for purge");
     pcb.add_u64_counter(l_mdc_strays_reintegrated, "strays_reintegrated", "Stray dentries reintegrated");
     pcb.add_u64_counter(l_mdc_strays_migrated, "strays_migrated", "Stray dentries migrated");
+
 
     /* Recovery queue statistics */
     pcb.add_u64(l_mdc_num_recovering_processing, "num_recovering_processing", "Files currently being recovered");
