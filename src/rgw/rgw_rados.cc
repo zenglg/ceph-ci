7997d93dd5496ec46149402ddd6ff1e9f615c468
Matt Benjamin <mbenjamin@redhat.com>
2017-01-05 10:45:15 -0500

    rgw:  try-extra ref on get_obj_data
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -1,4 +1,3 @@
-
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 
@@ -9867,6 +9866,12 @@ int RGWRados::get_obj_iterate_cb(RGWObjectCtx *ctx, RGWObjState *astate,
 
   int r;
 
+  /* try-check d */
+  ldout(cct, 0) << "rados->get_obj_iterate_cb oid=" << oid << " obj-ofs="
+				<< obj_ofs << " read_ofs=" << read_ofs << " len=" << len
+				<< " d->nref: " << d->get_nref()
+				<< dendl;
+
   if (is_head_obj) {
     /* only when reading from the head object do we need to do the atomic test */
     r = append_atomic_test(rctx, obj, op, &astate);
@@ -9920,8 +9925,9 @@ int RGWRados::get_obj_iterate_cb(RGWObjectCtx *ctx, RGWObjState *astate,
 
   // Flush data to client if there is any
   r = flush_read_list(d);
-  if (r < 0)
+  if (r < 0) {
     return r;
+  }
 
   return 0;
 
@@ -9937,9 +9943,13 @@ int RGWRados::Object::Read::iterate(int64_t ofs, int64_t end, RGWGetDataCB *cb)
 {
   RGWRados *store = source->get_store();
   CephContext *cct = store->ctx();
+  bool done = false;
 
   struct get_obj_data *data = new get_obj_data(cct);
-  bool done = false;
+
+  /* XXX debug special magic */
+  data->get(); // extra ref
+  cb->set_op_data(data); // and save it
 
   RGWObjectCtx& obj_ctx = source->get_ctx();
 
@@ -9980,7 +9990,7 @@ int RGWRados::iterate_obj(RGWObjectCtx& obj_ctx, rgw_obj& obj,
 	                  void *arg)
 {
   rgw_bucket bucket;
-  rgw_obj read_obj = obj;
+  rgw_obj read_obj = obj; // XXX we wanted to make a copy?
   uint64_t read_ofs = ofs;
   uint64_t len;
   bool reading_from_head = true;
@@ -13011,5 +13021,11 @@ int rgw_compression_info_from_attrset(map<string, bufferlist>& attrs, bool& need
   } else {
     need_decompress = false;
     return 0;
+  }
+}
+
+RGWGetDataCB::~RGWGetDataCB() {
+  if (op_data) {
+    op_data->put();
   }
 }
