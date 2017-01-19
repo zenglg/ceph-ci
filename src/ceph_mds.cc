773d949e3ddbedb37108f62f8dc7f2000aa3d7b2
Sage Weil <sage@redhat.com>
2017-01-19 08:54:35 -0600

    mds: tell crypto subsystem not to assert on leaked crypto objects on shutdown
    This doesn't matter for the MDS because it doesn't have to restart the
    crypto library within the same process.  If/when it ever does, this
    will need to be fixed.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/ceph_mds.cc
+++ src/ceph_mds.cc
@@ -26,6 +26,7 @@ using namespace std;
 
 #include "common/config.h"
 #include "common/strtol.h"
+#include "common/ceph_crypto.h"
 
 #include "mon/MonMap.h"
 #include "mds/MDSDaemon.h"
@@ -95,6 +96,11 @@ int main(int argc, const char **argv)
 #endif
 {
   ceph_pthread_setname(pthread_self(), "ceph-mds");
+
+  // the MDS doesn't attempt to clean up it's memory footprint on shutdown.
+  // until it does, we have to tell the crypto subsystem the same or else
+  // NSS will be unable to shut down cleanly and will assert out.
+  g_allow_leaky_crypto_shutdown = 1;
 
   vector<const char*> args;
   argv_to_vec(argc, argv, args);
