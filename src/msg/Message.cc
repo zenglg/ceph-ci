98c23813617f104c1f6a4ab7f4da3793272ae058
Sage Weil <sage@redhat.com>
2017-02-10 14:05:26 -0500

    osd: use new MOSDRepScrubMap message for ScrubMap instead of MOSDSubOp
    This is one of the last remaining users of MOSDSubOp!
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/msg/Message.cc
+++ src/msg/Message.cc
@@ -80,6 +80,7 @@ using namespace std;
 #include "messages/MOSDPGTrim.h"
 #include "messages/MOSDScrub.h"
 #include "messages/MOSDRepScrub.h"
+#include "messages/MOSDRepScrubMap.h"
 #include "messages/MOSDPGScan.h"
 #include "messages/MOSDPGBackfill.h"
 #include "messages/MOSDBackoff.h"
@@ -507,6 +508,9 @@ Message *decode_message(CephContext *cct, int crcflags,
     break;
   case MSG_OSD_REP_SCRUB:
     m = new MOSDRepScrub;
+    break;
+  case MSG_OSD_REP_SCRUBMAP:
+    m = new MOSDRepScrubMap;
     break;
   case MSG_OSD_PG_SCAN:
     m = new MOSDPGScan;
