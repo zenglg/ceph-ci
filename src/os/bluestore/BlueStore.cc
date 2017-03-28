2723c7c3f0ab87def0825903aaf332ff67ac55e1
J. Eric Ivancich <ivancich@redhat.com>
2017-03-28 13:34:26 -0400

    doc: update copyright notice and tell emacs preferred code formatting
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -1,8 +1,9 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 /*
  * Ceph - scalable distributed file system
  *
- * Copyright (C) 2014 Red Hat
+ * Copyright (C) 2017 Red Hat
  *
  * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -3,7 +3,7 @@
 /*
  * Ceph - scalable distributed file system
  *
- * Copyright (C) 2014 Red Hat
+ * Copyright (C) 2017 Red Hat
  *
  * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
