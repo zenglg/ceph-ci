ceph/ceph

rgw multisite: feature of bucket sync enable/disable
use bucket info'flag to store bucket sync flag, before adding the data log, we check the bucket flag.

add two rgw-admin command
radosgw-admin bucket sync enable
radosgw-admin bucket sync disable

Signed-off-by: Zengran Zhang <zhangzengran@h3c.com>

src/rgw/rgw_cr_rados.cc
@@ -202,6 +202,16 @@ RGWAsyncUnlockSystemObj::RGWAsyncUnlockSystemObj(RGWCoroutine *caller, RGWAioCom
 {
 }
 
+int RGWAsyncDeleteSystemObj::_send_request()
+{
+    return store->delete_system_obj(obj);  
+}
+
+RGWAsyncDeleteSystemObj::RGWAsyncDeleteSystemObj(RGWCoroutine *caller, RGWAioCompletionNotifier *cn, RGWRados *_store,
+                            RGWObjVersionTracker *_objv_tracker, rgw_obj& _obj) : RGWAsyncRadosRequest(caller, cn), store(_store), 
+    objv_tracker(_objv_tracker), obj(_obj)
+{
+}
 
 RGWRadosSetOmapKeysCR::RGWRadosSetOmapKeysCR(RGWRados *_store,
                       rgw_bucket& _pool, const string& _oid,
@@ -398,6 +408,19 @@ int RGWSimpleRadosUnlockCR::request_complete()
   return req->get_ret_status();
 }
 
+int RGWSimpleRadosDeleteCR::send_request()
+{
+  rgw_obj obj = rgw_obj(pool, oid);
+  req = new RGWAsyncDeleteSystemObj(this, stack->create_completion_notifier(),
+				    store, NULL, obj);
+  async_rados->queue(req);
+  return 0;
+}
+
+int RGWSimpleRadosDeleteCR::request_complete()
+{
+    return req->get_ret_status();
+}
 
 int RGWOmapAppend::operate() {
   reenter(this) {

src/rgw/rgw_rados.h
@@ -2966,7 +2966,7 @@ public:
 
   int decode_policy(bufferlist& bl, ACLOwner *owner);
   int get_bucket_stats(rgw_bucket& bucket, int shard_id, string *bucket_ver, string *master_ver,
-      map<RGWObjCategory, RGWStorageStats>& stats, string *max_marker);
+      map<RGWObjCategory, RGWStorageStats>& stats, string *max_marker, bool* syncstopped = NULL);
   int get_bucket_stats_async(rgw_bucket& bucket, int shard_id, RGWGetBucketStats_CB *cb);
   int get_user_stats(const rgw_user& user, RGWStorageStats& stats);
   int get_user_stats_async(const rgw_user& user, RGWGetUserStats_CB *cb);
@@ -3012,6 +3012,8 @@ public:
   int cls_bucket_head_async(rgw_bucket& bucket, int shard_id, RGWGetDirHeader_CB *ctx, int *num_aio);
   int list_bi_log_entries(rgw_bucket& bucket, int shard_id, string& marker, uint32_t max, std::list<rgw_bi_log_entry>& result, bool *truncated);
   int trim_bi_log_entries(rgw_bucket& bucket, int shard_id, string& marker, string& end_marker);
+  int resync_bi_log_entries(rgw_bucket& bucket, int shard_id);
+  int stop_bi_log_entries(rgw_bucket& bucket, int shard_id);
   int get_bi_log_status(rgw_bucket& bucket, int shard_id, map<int, string>& max_marker);
 
   int bi_get_instance(rgw_obj& obj, rgw_bucket_dir_entry *dirent);

zenglg/ceph-ci

src/rgw/rgw_cr_rados.cc
@@ -201,6 +201,16 @@ RGWAsyncUnlockSystemObj::RGWAsyncUnlockSystemObj(RGWCoroutine *caller, RGWAioCom
 {
 }
 
+int RGWAsyncDeleteSystemObj::_send_request()
+{
+    return store->delete_system_obj(obj);  
+}
+
+RGWAsyncDeleteSystemObj::RGWAsyncDeleteSystemObj(RGWCoroutine *caller, RGWAioCompletionNotifier *cn, RGWRados *_store,
+                            RGWObjVersionTracker *_objv_tracker, rgw_raw_obj& _obj) : RGWAsyncRadosRequest(caller, cn), store(_store),
+    objv_tracker(_objv_tracker), obj(_obj)
+{
+}
 
 RGWRadosSetOmapKeysCR::RGWRadosSetOmapKeysCR(RGWRados *_store,
                       const rgw_raw_obj& _obj,
@@ -420,6 +430,18 @@ int RGWSimpleRadosUnlockCR::request_complete()
   return req->get_ret_status();
 }
 
+int RGWSimpleRadosDeleteCR::send_request()
+{
+  req = new RGWAsyncDeleteSystemObj(this, stack->create_completion_notifier(),
+				    store, NULL, obj);
+  async_rados->queue(req);
+  return 0;
+}
+
+int RGWSimpleRadosDeleteCR::request_complete()
+{
+    return req->get_ret_status();
+}
 
 int RGWOmapAppend::operate() {
   reenter(this) {

src/rgw/rgw_rados.h
@@ -3358,7 +3358,7 @@ public:
 
   int decode_policy(bufferlist& bl, ACLOwner *owner);
   int get_bucket_stats(RGWBucketInfo& bucket_info, int shard_id, string *bucket_ver, string *master_ver,
-      map<RGWObjCategory, RGWStorageStats>& stats, string *max_marker);
+      map<RGWObjCategory, RGWStorageStats>& stats, string *max_marker, bool* syncstopped = NULL);
   int get_bucket_stats_async(RGWBucketInfo& bucket_info, int shard_id, RGWGetBucketStats_CB *cb);
   int get_user_stats(const rgw_user& user, RGWStorageStats& stats);
   int get_user_stats_async(const rgw_user& user, RGWGetUserStats_CB *cb);
@@ -3404,6 +3404,8 @@ public:
   int cls_bucket_head_async(const RGWBucketInfo& bucket_info, int shard_id, RGWGetDirHeader_CB *ctx, int *num_aio);
   int list_bi_log_entries(RGWBucketInfo& bucket_info, int shard_id, string& marker, uint32_t max, std::list<rgw_bi_log_entry>& result, bool *truncated);
   int trim_bi_log_entries(RGWBucketInfo& bucket_info, int shard_id, string& marker, string& end_marker);
+  int resync_bi_log_entries(RGWBucketInfo& bucket_info, int shard_id);
+  int stop_bi_log_entries(RGWBucketInfo& bucket_info, int shard_id);
   int get_bi_log_status(RGWBucketInfo& bucket_info, int shard_id, map<int, string>& max_marker);
 
   int bi_get_instance(const RGWBucketInfo& bucket_info, rgw_obj& obj, rgw_bucket_dir_entry *dirent);